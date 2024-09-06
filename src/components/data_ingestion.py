edbcab102a05357775b8a6d0f2d5f68e475b3b3c
MERAJ SHEIKH <142669482+MERAJSHEIKHH@users.noreply.github.com>
2024-09-06 16:38:34 +0530

    Add files via upload

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,73 @@
+
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+
+# initializing the data ingestion configuration 
+
+@dataclass
+class DataIngestionconfig:
+    train_data_path=os.path.join('artifacts','train.csv')
+    test_data_path=os.path.join('artifacts','test.csv')
+    raw_data_path=os.path.join('artifacts','raw.csv')
+    
+    
+    
+
+
+## create a data ingestion class
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Data Ingestion method starts')
+
+        try:
+            df=pd.read_csv(os.path.join(r"C:\Users\MERAJ UL ARFIN\Desktop\ml project\diamond\notebook\data","gemstone.csv"))
+            logging.info('Dataset read as pandas Dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+
+            logging.info("Train test split")
+            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+
+
+
+        except Exception as e:
+            logging.info('Error occured in Data Ingestion config')
+
+            
+        
+            
+            
+        """
+            Method Name :   initiate_data_ingestion
+            Description :   This method initiates the data ingestion components of training pipeline 
+            
+            Output      :   train set and test set are returned as the artifacts of data ingestion components
+            On Failure  :   Write an exception log and then raise an exception
+            
+            Version     :   1.2
+            Revisions   :   moved setup to cloud
+        """
+       
